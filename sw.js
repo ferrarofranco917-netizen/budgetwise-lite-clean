3974ae02b25f8cff93c5fda4e115d2e288dbc96a
ferrarofranco917-netizen <ferrarofranco917@gmail.com>
2026-02-22 17:16:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'budgetwise-v6';
+const CACHE_NAME = 'budgetwise-v7';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -121,6 +121,7 @@ self.addEventListener('notificationclick', event => {
         );
     }
 });
+
 
 
 
